3a2b87c214d7b7ec294e1ed333c5e21e6ba87a9d
Pedro Machado Santa <pedro.santa@gmail.com>
2017-04-19 11:50:12 +0100

    Remove unnecessary users route

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -28,8 +25,7 @@ app.use(require('node-sass-middleware')({
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules/quill/dist')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(require('./controllers'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
