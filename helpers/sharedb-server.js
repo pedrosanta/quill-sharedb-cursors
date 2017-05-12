33ba0535b53c580d91b6b2de3d8b919ae3d5c781
Pedro Machado Santa <pedro.santa@gmail.com>
2017-05-12 07:07:18 +0100

    Add MONGODB_URI env var config

--- helpers/sharedb-server.js
+++ helpers/sharedb-server.js
@@ -3,5 +3,5 @@ var ShareDB = require('sharedb');
 ShareDB.types.register(require('rich-text').type);
 
 module.exports = new ShareDB({
-  db: require('sharedb-mongo')('mongodb://localhost:27017/quill-sharedb-cursors')
+  db: require('sharedb-mongo')(process.env.MONGODB_URI || 'mongodb://localhost/quill-sharedb-cursors?auto_reconnect=true')
 });
