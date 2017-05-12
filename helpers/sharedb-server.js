10f2a3615e14902b7e3e5c6809a206fd91333ab9
Pedro Machado Santa <pedro.santa@gmail.com>
2017-05-12 03:33:06 +0100

    Add communication via socket.io (not working, falling back to ws)

--- helpers/sharedb-server.js
+++ helpers/sharedb-server.js
@@ -1,5 +1,7 @@
 var ShareDB = require('sharedb');
 
+ShareDB.types.register(require('rich-text').type);
+
 module.exports = new ShareDB({
   db: require('sharedb-mongo')('mongodb://localhost:27017/quill-sharedb-cursors')
 });
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,45 @@
+var ShareDB = require('sharedb/lib/client');
+var Quill = require('quill');
+
+ShareDB.types.register(require('rich-text').type);
+
+var socket = io.connect('http://localhost:3000/ot', {
+  upgrade: false,
+  transports: ['websocket']
+});
+
+var shareDBConnection = new ShareDB.Connection(socket);
+
 var quill = new Quill('#editor', {
   theme: 'snow'
+});
+
+var doc = shareDBConnection.get('documents', 'foobar');
+
+doc.subscribe(function (err) {
+
+  if(err) throw err;
+
+  if(!doc.type)
+    doc.create([{insert: '\n'}], 'rich-text');
+
+  // update editor contents
+  quill.setContent(doc.data);
+
+  // local -> server
+  quill.on('text-change', function(delta, oldDelta, source) {
+    console.log(delta, oldDelta, source);
+    if(source == 'user') {
+      doc.submitOp(delta, {source: quill}, function (err) {
+        if(err)
+          console.error('Submit OP returned an error:', err);
+      });
+    }
+  });
+
+  // server -> local
+  doc.on('op', function(op, source) {
+    if (source !== quill);
+      quill.updateContents(op);
+  });
 });
